8f1436e1df02219bc7cd93638e3c2ce9b7c3a3f1
Daniel Arruda <danielp.arruda@gmail.com>
2023-08-29 13:30:15 -0300

    Laboratório 1 - Inicialização do vetor com números randômicos

--- laboratorios/1/atividade_5.c
+++ laboratorios/1/atividade_5.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <pthread.h>
+#include <time.h>
 
 #define NTHREADS 8
 #define NELEMENTOS 10000
+#define VALORMAX 10000
 
 // Funções:
 // 1. Função inicializar vetor: vetor[i] = i
@@ -28,7 +30,36 @@ void* ElevarAoQuadrado(void *arg) {
     pthread_exit(NULL);
 }
 
-long int* InicializarVetor(int tamanho) {
+long int* InicializarVetor(int tamanho, int valorMaximo) {
+    long int* vetor;
+
+    vetor = malloc(tamanho * sizeof(long int));
+
+    if (vetor == NULL) {
+      printf("ERRO: malloc()\n");
+      exit(-1);
+    }
+
+    srand((unsigned) time(NULL));
+
+    for(int i = 0; i < tamanho; i++)
+        vetor[i] = rand() % valorMaximo;
+    
+    return vetor;
+}
+
+int VerificarResultado(long int* vetorOriginal, long int* vetorModificado, int tamanho) {
+    for(int i = 0; i < tamanho; i++) {
+        // printf("%ld %ld\n", vetorOriginal[i], vetorModificado[i]);
+
+        if(vetorModificado[i] != (vetorOriginal[i] * vetorOriginal[i]))
+            return 0;
+    }
+
+    return 1;
+}
+
+long int* CopiarVetor(long int* vetorOriginal, int tamanho) {
     long int* vetor;
 
     vetor = malloc(tamanho * sizeof(long int));
@@ -39,25 +70,17 @@ long int* InicializarVetor(int tamanho) {
     }
 
     for(int i = 0; i < tamanho; i++)
-        vetor[i] = i;
+        vetor[i] = vetorOriginal[i];
     
     return vetor;
-}
-
-int VerificarResultado(long int* vetor, int tamanho) {
-    for(int i = 0; i < tamanho; i++) {
-        if(vetor[i] != (i * i))
-            return 0;
-    }
-
-    return 1;
 }
 
 int main() {
     pthread_t tid_sistema[NTHREADS];
     t_Args* args;
 
-    long int* vetor = InicializarVetor(NELEMENTOS);
+    long int* vetor = InicializarVetor(NELEMENTOS, VALORMAX);
+    long int* vetorModificado = CopiarVetor(vetor, NELEMENTOS);
 
     // Inicializando args
     args = malloc(NTHREADS * sizeof(t_Args));
@@ -68,7 +91,7 @@ int main() {
     }
 
     for(int i = 0; i < NTHREADS; i++) 
-        args[i].vetor = vetor;  
+        args[i].vetor = vetorModificado;  
 
     // Abordagem 1
     // for(int i = 0; i < NTHREADS; i++) {
@@ -85,7 +108,7 @@ int main() {
     // Abordagem 2
     for(int i = 0; i < NTHREADS; i++) {
         args[i].inicio = i;
-        args[i].fim = NELEMENTOS;
+        args[i].fim = NELEMENTOS - 1;
         args[i].salto = NTHREADS;
     
         if (pthread_create(&tid_sistema[i], NULL, ElevarAoQuadrado, (void*) &args[i])) {
@@ -103,13 +126,14 @@ int main() {
     }
 
     // Verifica o resultado
-    if(VerificarResultado(vetor, NELEMENTOS))
+    if(VerificarResultado(vetor, vetorModificado, NELEMENTOS))
         printf("Valores corretos\n");
     else
         printf("Valores incorretos\n");
 
     free(args);
     free(vetor);
+    free(vetorModificado);
 
     pthread_exit(NULL);
 }
